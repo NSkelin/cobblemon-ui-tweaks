246fc9692bfbdab697784639290a8c6f09f0cd63
Nick <nick.skelin@gmail.com>
2025-03-09 06:40:40 -0700

    add config to reverse pc scroll direction

--- src/client/java/com/cobblemonuiextensions/config/ModConfig.java
+++ src/client/java/com/cobblemonuiextensions/config/ModConfig.java
@@ -7,4 +7,5 @@ import io.wispforest.owo.config.annotation.Modmenu;
 @Config(name = "CobblemonUIExtensions", wrapperName = "CobblemonUIExtensionsConfig")
 public class ModConfig {
     public boolean ShowBattleTypes = true;
+    public boolean ReversePCScrollDirection = false;
 }
--- src/client/java/com/cobblemonuiextensions/mixin/client/PCGUIMixin.java
+++ src/client/java/com/cobblemonuiextensions/mixin/client/PCGUIMixin.java
@@ -2,6 +2,7 @@ package com.cobblemonuiextensions.mixin.client;
 
 import com.cobblemon.mod.common.client.gui.pc.PCGUI;
 import com.cobblemon.mod.common.client.gui.pc.StorageWidget;
+import com.cobblemonuiextensions.CobblemonUIExtensionsClient;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
@@ -18,8 +19,9 @@ public abstract class PCGUIMixin {
     private void injectPCScrollControl(
             double mouseX, double mouseY, double amount, double verticalAmount, CallbackInfoReturnable<Boolean> cir
     ) {
+        var reverse = CobblemonUIExtensionsClient.CONFIG.ReversePCScrollDirection() ? 1 : -1;
         int box = this.storageWidget.getBox();
-        int change = (int) (verticalAmount * -1);
+        int change = (int) (verticalAmount * reverse);
         this.storageWidget.setBox(box + change);
     }
 
