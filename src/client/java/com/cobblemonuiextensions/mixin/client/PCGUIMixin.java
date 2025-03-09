31829222ae673d3980c7f8b226bf8feb1585fd72
Nick <nick.skelin@gmail.com>
2025-03-09 06:01:03 -0700

    invert PC scroll direction
    I prefer it this way so it shall be the default

--- src/client/java/com/cobblemonuiextensions/mixin/client/PCGUIMixin.java
+++ src/client/java/com/cobblemonuiextensions/mixin/client/PCGUIMixin.java
@@ -18,9 +18,12 @@ public abstract class PCGUIMixin {
     private void injectPCScrollControl(
             double mouseX, double mouseY, double amount, double verticalAmount, CallbackInfoReturnable<Boolean> cir
     ) {
-        this.storageWidget.setBox((int) (this.storageWidget.getBox() + verticalAmount));
+        int box = this.storageWidget.getBox();
+        int change = (int) (verticalAmount * -1);
+        this.storageWidget.setBox(box + change);
     }
 
+    // fix box opening on slot 2 instead of 1
     @Inject(method="init", at = @At("TAIL"))
     private void injectInit(CallbackInfo ci) {
         this.storageWidget.setBox(0);
