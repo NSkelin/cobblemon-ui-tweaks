a6993e0fd5065374ab2bee792665c9b04238377a
Nick <nick.skelin@gmail.com>
2025-03-08 19:01:27 -0800

    add pc / pasture page change scroll support

---
+++ src/client/java/com/cobblemonuitweaks/mixin/client/PCGUIMixin.java
@@ -0,0 +1,22 @@
+package com.cobblemonuitweaks.mixin.client;
+
+import com.cobblemon.mod.common.client.gui.pc.PCGUI;
+import com.cobblemon.mod.common.client.gui.pc.StorageWidget;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+
+@Mixin(PCGUI.class)
+public abstract class PCGUIMixin {
+
+    @Shadow private StorageWidget storageWidget;
+
+    @Inject(method = "mouseScrolled", at = @At("TAIL"))
+    private void injectPCScrollControl(
+            double mouseX, double mouseY, double amount, double verticalAmount, CallbackInfoReturnable<Boolean> cir
+    ) {
+        this.storageWidget.setBox((int) (this.storageWidget.getBox() + verticalAmount));
+    }
+}
